d754bbf59d2f25aff31fcf45cefd395f8e0790d3
Rachana Acharya <rasnacharya@gmail.com>
2017-04-03 23:13:49 -0600

    Learning about various ways of using a variable

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
+	$name = 'Rachana Acharya';
 	$greeting = "Hello World!";
-	echo $greeting; 
+	//echo $greeting;
+	//echo ' Hello, $name '; // variable can't be used with single quotes
+	//echo "Hello, $name"; // variable can be used with double quotes
+	echo "Hello, " . $name; //dot means string concatenation
+	echo "Hello, {$name}"; //most common
