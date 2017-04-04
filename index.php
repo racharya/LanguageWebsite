9c5bcd12b4944cf19eb84bb56ea75304c9eb46cb
Rachana Acharya <rasnacharya@gmail.com>
2017-04-04 13:49:27 -0600

    Play with html within php and apply some styling

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<html>
+<header>
+<title>Team Gull</title>
+<style>
+h1{
+	background: #D01031;
+	text-align: center;
+	padding: 2em;
+	}
+</style>
+</header>
+<body>
+<h1>Hello World</h1>
+</body>
+</html>
 <?php
 	$name = 'Rachana Acharya';
 	$greeting = "Hello World!";
@@ -5,4 +20,4 @@
 	//echo ' Hello, $name '; // variable can't be used with single quotes
 	//echo "Hello, $name"; // variable can be used with double quotes
 	echo "Hello, " . $name; //dot means string concatenation
-	echo "Hello, {$name}"; //most common
+	//echo "Hello, {$name}"; //most common
