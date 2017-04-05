7476c52311ea7b98b6fa7fa71bb1bc9d38758c3e
Rachana Acharya <rasnacharya@gmail.com>
2017-04-05 02:29:05 -0600

    contains html with php in it, separation of concerns

---
+++ index.view.php
@@ -0,0 +1,18 @@
+<html>
+<header>
+<title>Team Gull</title>
+<style>
+h1{
+	background: #D01031;
+	text-align: center;
+	padding: 2em;
+	}
+</style>
+</header>
+<body>
+	<h1>
+		<?= $greeting; ?>
+	</h1>		
+</body>
+</html>
+
