a53a70a7d2ca3c530f842610c57a704870dc5732
Rachana Acharya <rasnacharya@gmail.com>
2017-04-05 02:28:34 -0600

    contains only php stuff now, separation of responsibility

--- index.php
+++ index.php
@@ -1,22 +1,3 @@
-<html>
-<header>
-<title>Team Gull</title>
-<style>
-h1{
-	background: #D01031;
-	text-align: center;
-	padding: 2em;
-	}
-</style>
-</header>
-<body>
-	<h1>
-			<?= "Hello, ". $_GET['name']; //short hand of doing the following
-			// $name = $_GET['name'];
-			// echo $name;
-			//echo "Hello World!"; 
-		?>
-	</h1>		
-</body>
-</html>
-
+<?php
+$greeting = "Hello World!";
+require 'index.view.php';
