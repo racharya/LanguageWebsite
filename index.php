96bd7b0198e730c9988e75c04d9b04e75928c281
Rachana Acharya <rasnacharya@gmail.com>
2017-04-05 01:59:04 -0600

    Experimenting with html + php

--- index.php
+++ index.php
@@ -10,14 +10,12 @@ h1{
 </style>
 </header>
 <body>
-<h1>Hello World</h1>
+	<?php
+		$greetings = "Hello World!";
+		echo $greetings;
+		?>
+		
+			
 </body>
 </html>
-<?php
-	$name = 'Rachana Acharya';
-	$greeting = "Hello World!";
-	//echo $greeting;
-	//echo ' Hello, $name '; // variable can't be used with single quotes
-	//echo "Hello, $name"; // variable can be used with double quotes
-	echo "Hello, " . $name; //dot means string concatenation
-	//echo "Hello, {$name}"; //most common
+
